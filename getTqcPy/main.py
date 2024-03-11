21c11ae2f86a6414c691b3e4c9730523833230d3
lucashsu95 <lucashsu95gmail.com>
2024-03-11 15:44:15 +0800

    get TQC pyhton

---
+++ getTqcPy/main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from FunPy import getDetail
+
+
+url = 'https://jbprogramnotes.com/2020/05/tqc-%e7%a8%8b%e5%bc%8f%e8%aa%9e%e8%a8%80python-%e7%ac%ac2%e9%a1%9e%ef%bc%9a%e9%81%b8%e6%93%87%e6%95%98%e8%bf%b0/'
+response = requests.get(url)
+time.sleep(1)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+a_tags = soup.select('div.post-content > p:nth-of-type(2) a')
+
+if a_tags:
+    for a_tag in a_tags:
+        link = a_tag['href']
+        getDetail(link)
+        time.sleep(5)
+
+else:
+    print("找不到符合條件的元素。")
