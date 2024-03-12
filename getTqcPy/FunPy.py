e4382fddd0a551a15db1150d419922c641f75377
lucashsu95 <lucashsu95gmail.com>
2024-03-12 20:26:52 +0800

    fix Python tqc Funpy.py

--- getTqcPy/FunPy.py
+++ getTqcPy/FunPy.py
@@ -12,18 +12,29 @@ def getDetail(url):
     h1_tags = soup.select_one('h1').text
     h1 = h1_tags.split()
     # 這裡可能會有問題
-    os.path.join('topic',h1[2],h1[2] + '.md')
+    folder_path = f'topic/{h1[2]}'
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+        print(f"已創建資料夾：{folder_path}")
+    else:
+        print(f"資料夾已存在：{folder_path}")
+
     with open(f'topic/{h1[2]}/{h1[2]}.md' , 'w',encoding='utf-8') as f:
         p = soup.select_one('div.post-content > p').text
         div = soup.select_one('div.post-content > div').text
         wpHead = soup.select('div.post-content > .wp-block-heading')
         wpBlock = soup.select('div.post-content > .wp-block-code')
+        blockquote = soup.select_one('div.post-content > blockquote')
 
         f.write('# ' + h1_tags)
         f.write('\n')
         f.write(p)
         f.write(div)
         f.write('\n')
+
+        if blockquote:
+            f.write('> ' + blockquote.text)
+            f.write('\n')
 
         flag = 1
         for i,j in zip(wpHead,wpBlock[:-1]):
@@ -35,9 +46,8 @@ def getDetail(url):
             f.write('```')
             f.write('\n')
 
-            if i.text == '範例輸入':
+            if i.text == '範例輸入' or i.text[:-1] == '範例輸入':
                 with open(f'topic/{h1[2]}/in{flag}.txt', 'a+', encoding='utf-8') as f2: f2.write(j.text)
             else:
                 with open(f'topic/{h1[2]}/out{flag}.txt', 'a+', encoding='utf-8') as f2: f2.write(j.text)
-
-            flag += 1
+                flag += 1
--- getTqcPy/main.py
+++ getTqcPy/main.py
@@ -4,7 +4,7 @@ import time
 from FunPy import getDetail
 
 
-url = 'https://jbprogramnotes.com/2020/05/tqc-%e7%a8%8b%e5%bc%8f%e8%aa%9e%e8%a8%80python-%e7%ac%ac2%e9%a1%9e%ef%bc%9a%e9%81%b8%e6%93%87%e6%95%98%e8%bf%b0/'
+url = 'https://jbprogramnotes.com/2020/05/tqc-%e7%a8%8b%e5%bc%8f%e8%aa%9e%e8%a8%80python-%e7%ac%ac4%e9%a1%9e%ef%bc%9a%e9%80%b2%e9%9a%8e%e6%8e%a7%e5%88%b6%e6%b5%81%e7%a8%8b/'
 response = requests.get(url)
 time.sleep(1)
 soup = BeautifulSoup(response.text, 'html.parser')
